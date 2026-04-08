82d84d977beadf6c4e2023cedc47b2f32e95a8a0
Andriy Gavrylenko <gavrosh2007@gmail.com>
2026-04-08 14:11:27 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -19,12 +19,7 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
 
